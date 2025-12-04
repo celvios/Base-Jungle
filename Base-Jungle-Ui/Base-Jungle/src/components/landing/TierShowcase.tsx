a115a03b3f9207b6ee0021080d22d5fd01be5046
Base Jungle Dev <dev@basejungle.xyz>
2025-12-04 11:32:35 +0100

    Revert mobile adaptation for tiers to use MobileGallery

--- Base-Jungle-Ui/Base-Jungle/src/components/landing/TierShowcase.tsx
+++ Base-Jungle-Ui/Base-Jungle/src/components/landing/TierShowcase.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import HolographicComparison from './SpecimenGallery/HolographicComparison';
+import MobileGallery from './SpecimenGallery/MobileGallery';
 
 const TierShowcase: React.FC = () => {
     return (
-        <HolographicComparison />
+        <>
+            {/* Desktop View */}
+            <div className="hidden md:block">
+                <HolographicComparison />
+            </div>
+
+            {/* Mobile View */}
+            <div className="block md:hidden">
+                <MobileGallery />
+            </div>
+        </>
     );
 };
 
