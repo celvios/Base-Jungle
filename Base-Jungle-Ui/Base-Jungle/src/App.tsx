300374c2a59bba271e4e86644a7b69c9dd0b506e
celvios <toluking001@gmail.com>
2026-02-02 16:54:02 +0100

    fix(routing): revert to standard routing

--- Base-Jungle-Ui/Base-Jungle/src/App.tsx
+++ Base-Jungle-Ui/Base-Jungle/src/App.tsx
@@ -1,5 +1,4 @@
 import { Switch, Route, useLocation, Link, Router as WouterRouter } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -150,24 +149,22 @@ function App() {
   };
 
   return (
-    // Use Hash Routing logic
-    <WouterRouter hook={useHashLocation}>
-      <ThemeProvider>
-        <WalletProvider>
-          <ModalProvider>
-            <QueryClientProvider client={queryClient}>
-              <TooltipProvider>
-                <SidebarProvider style={style as React.CSSProperties}>
-                  <ModalRenderer />
-                  <Toaster />
-                  <AppContent />
-                </SidebarProvider>
-              </TooltipProvider>
-            </QueryClientProvider>
-          </ModalProvider>
-        </WalletProvider>
-      </ThemeProvider>
-    </WouterRouter>
+    // Use Standard Routing logic
+    <ThemeProvider>
+      <WalletProvider>
+        <ModalProvider>
+          <QueryClientProvider client={queryClient}>
+            <TooltipProvider>
+              <SidebarProvider style={style as React.CSSProperties}>
+                <ModalRenderer />
+                <Toaster />
+                <AppContent />
+              </SidebarProvider>
+            </TooltipProvider>
+          </QueryClientProvider>
+        </ModalProvider>
+      </WalletProvider>
+    </ThemeProvider>
   );
 }
 
